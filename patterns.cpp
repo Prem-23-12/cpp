632db3e552e22149e3b823c97359194a490f4313
Prem-23-12 <premparmar.2005@gmail.com>
2025-09-11 05:25:53 -0700

    Changes

--- patterns.cpp
+++ patterns.cpp
@@ -1,16 +1,16 @@
  # include <iostream>
   using namespace std;
   int main(){ 
-    // int n;
-    // cout <<"enter n";
-    // cin >>n;
-    // for(int i=1;i<=n;i++){
-    //     char ch='A';
-    //     for(int j=1;j<=n;j++){
-    //        cout << ch << " ";
-    //        ch=ch+1;
-    //     }cout <<"\n";
-    // }
+    int n;
+    cout <<"enter n";
+    cin >>n;
+    for(int i=1;i<=n;i++){
+        char ch='A';
+        for(int j=1;j<=n;j++){
+           cout << ch << " ";
+           ch=ch+1;
+        }cout <<"\n";
+    }
     //   int n,i,j;
     // cout << "enter n ";
     // cin >> n;
