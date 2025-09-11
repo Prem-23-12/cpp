fee510d7b4d8fa3fc57510c32baf03d2cb1a9e5d
Prem-23-12 <premparmar.2005@gmail.com>
2025-09-11 04:31:32 -0700

    Loops Working

---
+++ loops.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main(){
+    // How to use for loop
+    /* for(initializtion ; condition ; increement/ Decrement){
+            Block of code 
+    }    */
+    for(int i=0 ; i<5 ;i++){ 
+        cout << i << endl;
+    }
+}
