700d2b12d4dc0169c77e32edbc766e3f7afd247b
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-02 01:17:28 +0530

    Files

--- patterns.cpp
+++ patterns.cpp
@@ -1,33 +1,6 @@
  # include <iostream>
   using namespace std;
   int main(){ 
-// Butterfly pattern
-
-//    int n,i,j ;
-//    cout << "enter n ";
-//    cin >> n;
-//    for(i=0;i<n;i++){
-//     for(j=0;j<i+1;j++){
-//         cout << "*";
-//     }  for(j=0;j<2*(n-i)-2;j++){
-//         cout << " ";
-//     }
-//    for(j=0;j<i+1;j++)
-//     {cout << "*";}
-//     cout << "\n";
-//    }
-//    for(i=0;i<n;i++){
-//     for(j=n-i;j>0;j--){
-//         cout << "*";
-//     }if(i!=0){
-//     for(j=2*i;j>0;j--){
-//         cout << " ";
-//     }}for(j=n-i;j>0;j--){
-//         cout << "*";}
-//     cout << "\n";
-//    }
-//   return 0;
-//   }
    int i,j,n;
    cout<<"enter n";
    cin>>n;
@@ -46,4 +19,5 @@
         else{cout <<n;}
     } cout <<endl;
    }
-   return 0;}
+   return 0;
+}
