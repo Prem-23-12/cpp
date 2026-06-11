b15a6d84397c14c4cc0e3f763499f1b09f4479b7
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-12 00:31:57 +0530

    file

--- Array/Move_zeros.cpp
+++ Array/Move_zeros.cpp
@@ -8,10 +8,7 @@ int main() {
     int n = nums.size();
     int j = 0 ;
     int k = 2 ;
-
-    for (int i = 1 ; i < n-1 ; i++){    
-        swap(k,n-k);
-    }
+    
     for (int x : nums){
         cout << x << " ";
     }
