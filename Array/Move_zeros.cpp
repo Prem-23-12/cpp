38cfbd9877a6e68d5c913024b579fbe2dbef6d55
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-11 10:09:49 +0530

    files

--- Array/Move_zeros.cpp
+++ Array/Move_zeros.cpp
@@ -1,16 +1,16 @@
 # include <iostream>
 # include <vector>
+# include <algorithm>
 using namespace std ; 
 
 int main() {
     vector<int> nums = {0,1,0,3,12};
     int n = nums.size();
-    int zero = 0;
+    int j = 0 ;
+    int k = 2 ;
 
-    for (int i = 0 ; i < n-1 ; i++){
-        if(nums[i] == zero){
-            nums.push_back(nums[i]);
-        }
+    for (int i = 1 ; i < n-1 ; i++){    
+        swap(k,n-k);
     }
     for (int x : nums){
         cout << x << " ";
