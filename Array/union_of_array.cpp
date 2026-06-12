f768021c05e9d63ae3ca8d45c7edfc2cdf3d9035
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-13 00:28:22 +0530

    file

--- Array/union_of_array.cpp
+++ Array/union_of_array.cpp
@@ -1,35 +1,53 @@
 # include <bits/stdc++.h>
 using namespace std ;
 
-void sorted_array(vector <int> &nums1, vector <int> &nums2){
+vector<int> Union(vector <int> &nums1, vector <int> &nums2){
     int n = nums1.size();
     int m = nums2.size();
-    int i = n - 1;
-    int j = m - 1;
-    int k = m + n - 1 ;
     
+    vector<int> result;
+    int i = 0, j = 0;
+    while(i < n && j < m){
+        if(nums1[i] < nums2[j]){
+            if(result.empty() || result.back() != nums1[i]){
+                result.push_back(nums1[i]);
+            }
+            i++;
+        }else if(nums1[i] > nums2[j]){
+            if(result.empty() || result.back() != nums2[j]){
+                result.push_back(nums2[j]);
+            }
+            j++;
+        }else {
+             if(result.empty() || result.back() != nums1[i]){
+                result.push_back(nums1[i]);
+             }
+             i++, j++;
+        }
+    }
+    while(i<n){
+        if(result.empty() || result.back() != nums1[i]){
+                result.push_back(nums1[i]);
+        }
+         i++;
+    }
+    while(j<m){
+        if(result.empty() || result.back() != nums2[j]){
+                result.push_back(nums2[j]);
+        }
+        j++;
+    }
 
-    while (i >= 0 && j >= 0){
-        if(nums1[i] > nums2[j]){
-            nums1[k] = nums1[i];
-            i--;
-        }
-        else {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
-    }
-    while(j >= 0){
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
+    return result;
 }
 
 int main(){
     vector<int> nums1 = {1,2,2,4,5};
     vector<int> nums2 = {3,3,4,5,6};
-    sorted_array(nums1, nums2);
+    vector<int> union_array = Union(nums1, nums2);
+    
+    for(int i : union_array){
+        cout << i << " ";
+    }
     return 0;
 }
