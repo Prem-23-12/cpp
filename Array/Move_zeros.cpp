b7f15b8664b2ac5c3bd10a25c7dd2e1d039ea938
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-12 09:25:18 +0530

    File

--- Array/Move_zeros.cpp
+++ Array/Move_zeros.cpp
@@ -17,7 +17,7 @@ void Move_zeros(vector<int> &nums){
      }
      if(j == -1) return ;
 
-     for(int i = 0 ; i < n ; i++){
+     for(int i = j+1 ; i < n ; i++){
             if(nums[i] != 0){
                 swap(nums[i],nums[j]);
                 j++ ;
