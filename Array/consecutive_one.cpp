a9793f3d11a3d2d18855dbc890db2127e3079804
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-13 14:44:55 +0530

    files

---
+++ Array/consecutive_one.cpp
@@ -0,0 +1,18 @@
+# include <bits/stdc++.h>
+using namespace std ;
+
+int consecutive_one(vector <int> &nums){
+    int n = nums.size();
+
+    int count = 0;
+    int max = 0;
+    for(int i = 0 ; i < n ; i++){
+        
+    }
+}
+
+int main(){
+    vector <int> nums = {1,1,0,1,1,1};
+
+    return 0;
+}
