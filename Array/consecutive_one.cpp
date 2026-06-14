ed9e875cdfe3bf9d483569f6b1011fa17a5dede0
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-14 19:16:00 +0530

    Files

--- Array/consecutive_one.cpp
+++ Array/consecutive_one.cpp
@@ -5,10 +5,18 @@ int consecutive_one(vector <int> &nums){
     int n = nums.size();
 
     int count = 0;
-    int max = 0;
+    int maxi = 0;
     for(int i = 0 ; i < n ; i++){
-        
+        if(nums[i] == 1){
+            count++ ;
+        }
+        else {
+            count  = 0;
+        }
     }
+    maxi = max(maxi,count);
+
+    return maxi;
 }
 
 int main(){
