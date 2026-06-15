d3b1fb7ad7f74077593bd680cddb54af06a195ef
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-15 14:23:54 +0530

    files

--- subset.cpp
+++ subset.cpp
@@ -3,20 +3,6 @@
 # include <string>
 using namespace std;
 
-void printsubset(vector<int> &arr, int i, vector<int> &ans){
-    if(i==arr.size()){
-        for(int val : ans){
-            cout << val << " "; 
-        }cout << endl;
-        return ;
-    }
-
-    ans.push_back(arr[i]);
-    printsubset(arr,i+1,ans);
-
-    ans.pop_back();
-    printsubset(arr,i+1,ans);
-}
 
 bool isSafe(vector<string> &board, int row, int col, int n){
 
@@ -65,8 +51,6 @@ void nQueens(vector<string> &board, int row, int n, vector<vector<string>> &ans)
 // sudoko solver 
     
 int main(){
-    vector<int> arr = {1,2,3};
-    vector<int> ans1;
     int n = 4 ;
     vector<string> board(n, string(n, '.'));
     vector<vector<string>> ans;
