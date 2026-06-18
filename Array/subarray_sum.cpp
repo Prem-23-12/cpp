4a949755080fbbd9a49f17fc6f6734e60617c41e
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-18 22:04:01 +0530

    sum

--- Array/subarray_sum.cpp
+++ Array/subarray_sum.cpp
@@ -1,22 +1,28 @@
 # include <bits/stdc++.h>
 using namespace std;
 
+
+// Brute force approach TC O(n^3)
 int subarray_sum(vector <int>& nums, int k){
     int n = nums.size();
     int maxsum = 0;
     for(int i = 0 ; i < n ; i++){
-        int cs = 0;
-        for(int j = 0 ; j < n ; j++){
-            cs = cs + nums[j];
-            maxsum = max(maxsum, cs);
+        for(int j = i ; j < n ; j++){
+            int cs = 0;
+            for(int k = i ; k <= j ; k++){
+                cs = cs + nums[i];
+            }
+            if(cs == k){
+                maxsum = max(maxsum, j-i+1);
+            }
         }
     }
     return maxsum;
 }
-
+    
 int main(){
     vector<int> nums = {1,2,3,4,5};
-    int k = 15;
-    subarray_sum(nums, k);
+    int k = 9;
+    cout << subarray_sum(nums, k);
     return 0;
 }
