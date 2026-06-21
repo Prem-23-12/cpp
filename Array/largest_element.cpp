b57d41b5564bce803474a98af85a11a1403e17b9
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-21 13:45:37 +0530

    files

---
+++ Array/largest_element.cpp
@@ -0,0 +1,17 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector <int> nums = {1,2,3,7,5,8,10};
+    int n = nums.size();
+
+    int max = INT_MIN;
+    
+    for (int i = 0 ; i < n ; i++){
+        if(nums[i] > max){
+            max = nums[i];
+        }
+    }
+
+    return 0;
+}
