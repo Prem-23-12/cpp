520546b876dc5a68b2f9c359f19e800de4495524
Prem-23-12 <premparmar.2005@gmail.com>
2026-06-24 23:11:02 +0530

    Two_sum

---
+++ Array/Two_sum.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+using namespace std ;
+
+vector <int> Two_sum(vector<int>& nums,int target){
+    int n = nums.size();
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i + 1 ; j < n ; j++){
+            if(nums[i]+nums[j] == target){
+                return {i,j};
+            }
+        }
+    }
+    return {};
+}
+
+int main (){
+    vector <int> nums = {2,7,11,15};
+    int target = 9 ;
+    Two_sum(nums, target);
+    for(int i : nums){
+        cout << i ;
+    }
+}
