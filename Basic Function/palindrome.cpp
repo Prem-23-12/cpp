5e341150edb8cada9375784c4305e380a1667dd5
Prem-23-12 <premparmar.2005@gmail.com>
2026-07-02 08:50:30 +0530

    files

--- Basic Function/palindrome.cpp
+++ Basic Function/palindrome.cpp
@@ -2,6 +2,18 @@
 using namespace std ;
 
 int main() {
-     
+    int n = 121;
+    int org = n ;
+    int rev = 0 ;
+    while(n > 0){
+        rev = rev * 10 + n % 10 ;
+        n /= 10 ;
+    }
+    if(rev == org){
+        cout << "Palindrome" << endl ;
+    }
+    else{
+        cout << "Not a Palindrome" << endl ;
+    }
     return 0 ;
 }
