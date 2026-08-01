4082fa1d28cf5634e31625ec5553fd7012bf4227
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-01 17:06:15 +0530

    palindrome

--- Strings/palindrome.cpp
+++ Strings/palindrome.cpp
@@ -1,6 +1,39 @@
 # include <bits/stdc++.h>
+# include <string>
 using namespace std;
 
+
+// check whether the string is palindrome or not 
+bool isAlphanumeric(char ch){
+        if((ch >= '0' && ch <='9')|| (tolower(ch) >='a' && tolower(ch) <= 'z')){
+            return true;
+        }
+        return false;
+ }
+ bool isPalindrome(string s){
+        int st=0, end = s.length()-1;
+
+        while(st < end) {
+            if(!isAlphanumeric(s[st])) {
+                st++; continue;
+            }
+            if(!isAlphanumeric(s[end])) {
+                end--; continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++; end--;
+        }
+        return true ; 
+     }
+
+
 int main(){
-    
+
+    string s = "abcfcba";
+
+    cout << isAlphanumeric(s) << endl ;
+    return 0 ; 
 }
