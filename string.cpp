d931392248363669e107c2ee7a0aacc95a30fc80
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-01 17:28:58 +0530

    delete

--- string.cpp
+++ string.cpp
@@ -3,40 +3,6 @@
 # include <vector>
 using namespace std;
 
-// check the permutation in the strings
-bool isFreqsame(int freq1[], int freq2[]){
-    for(int i=0 ; i<26 ; i++){
-        if(freq1[i] != freq2[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool ispermutation(string s1, string s2){
-    int freq[26]={0};
-
-    for(int i = 0 ; i<s1.length(); i++){
-        freq[s1[i]-'a']++;
-    }
-
-    int windSize= s1.length();
-
-    for(int i=0 ; i<s2.length(); i++){
-        int windIdx = 0,idx=i;
-        int windfreq[26]={0};
-
-        while(windIdx < windSize && idx < s2.length()){
-            windfreq[s2[idx]-'a']++;
-            windIdx++, idx++;
-        }
-        if(isFreqsame(freq, windfreq)){// found
-            return true;
-        }
-    }
-    return false;
-}
-
 // string reversethestr(string p){
 //     int n = p.length();
 //     string ans = "";
