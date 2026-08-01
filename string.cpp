5e94c2de4175594fa161a39886b4157ad73c9a4c
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-01 17:36:10 +0530

    Reverse

--- string.cpp
+++ string.cpp
@@ -3,24 +3,6 @@
 # include <vector>
 using namespace std;
 
-string reversethestr(string p){
-    int n = p.length();
-    string ans = "";
-    reverse(p.begin(),p.end());
-    for(int i = 0 ; i < n ; i++){
-        string word = ""; // length of word is zero
-        while (i<n && p[i]!=' '){
-            word += p[i];
-            i++;
-        } 
-        reverse(word.begin(),word.end());
-            if(word.length() > 0 ){
-               ans += " " + word;
-            }
-        }
-        return ans.substr(1);
-    }
-
 int compression(vector<char> chars){
     int n = chars.size();
     int idx=0;
