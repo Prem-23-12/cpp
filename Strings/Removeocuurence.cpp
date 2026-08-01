701f25821e1de773b4ceb14599d75518a5365833
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-01 17:22:50 +0530

    palindrome

--- Strings/Removeocuurence.cpp
+++ Strings/Removeocuurence.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+// Remove the occurences in the string      
+
+string removeoccurences(string str, string part){
+    while(str.length() >0 && str.find(part) < str.length()){
+        str.erase(str.find(part),part.length());
+    }
+    return str;
+}
+
+
+
 int main (){
+    string str ="dabcacbabcabc";
+    string part ="abc";
 
 
+    cout << removeoccurences(str, part) << endl;
     return 0 ;
 }
