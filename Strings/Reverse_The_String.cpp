cf8a3c04d49ef3297994f295f2b89cd39a165fc3
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-01 17:34:32 +0530

    Reverse

--- Strings/Reverse_The_String.cpp
+++ Strings/Reverse_The_String.cpp
@@ -1,6 +1,30 @@
 # include <bits/stdc++.h>
 using namespace std ;
 
+
+string reversethestr(string p){
+    int n = p.length();
+    string ans = "";
+    reverse(p.begin(),p.end());
+    for(int i = 0 ; i < n ; i++){
+        string word = ""; // length of word is zero
+        while (i<n && p[i]!=' '){
+            word += p[i];
+            i++;
+        } 
+        reverse(word.begin(),word.end());
+            if(word.length() > 0 ){
+               ans += " " + word;
+            }
+        }
+        return ans.substr(1);
+    }
+
+
 int main(){
-    
+    string p = " Myself Prem Parmar ";
+
+    cout << reversethestr(p) << endl ;
+
+    return 0 ;
 }
