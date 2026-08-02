dc36694fb97dd6919b9620757877d4cf321d9b27
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-02 22:08:25 +0530

    count f element

--- Strings/count_of_element.cpp
+++ Strings/count_of_element.cpp
@@ -1,9 +1,34 @@
 # include <bits/stdc++.h>
 using namespace std ;
 
+int compression(vector<char> chars){
+    int n = chars.size();
+    int idx=0;
+    for(int i = 0;i<n;i++){
+        char ch = chars[i];
+        int count = 0;
+
+        while(i<n && chars[i]==ch){
+            count++, i++;
+        }
+        if(count == 1){
+            chars[idx++]=ch;
+        }else {
+            chars[idx++]=ch;
+            string str = to_string(count);
+            for(char dig : str){
+                chars[idx++]=dig;
+            }
+        }i--;
+
+    }
+    chars.resize(idx);
+    return idx;
+}
 
 int main(){
-
-
-    return 0 ;
+    vector<char> chars = {'a','a','b','b','c','c','c'};
+    cout << compression(chars) ; 
+    
+    return 0;
 }
