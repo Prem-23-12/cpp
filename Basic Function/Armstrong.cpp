35a917352f5597b0f4c4abba800e5bcc5829c4c7
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-03 12:00:03 +0530

    file

--- Basic Function/Armstrong.cpp
+++ Basic Function/Armstrong.cpp
@@ -1,9 +1,23 @@
 # include <bits/stdc++.h>
 using namespace std ;
 
+// 
+
 int main(){
+    int n = 153 ;
+    int k = to_string(n).length();
+    int arm = 0 ;
+    int org = n ;
+    while (n != 0){
+        int i = n%10 ; 
+        arm = arm + pow(i,k);
+        n = n /10 ;
+    }
+    if(arm == org){
+        cout << "Armstrong number";
+    }else {
+        cout << "Not Armstrong number";
+    }
 
-
-    
     return 0 ;
 }
