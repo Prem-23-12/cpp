245cab611392890cec416321c58d379fc9b4a9f5
Prem-23-12 <premparmar.2005@gmail.com>
2026-08-06 23:07:19 +0530

    files

--- Recursion/print_n_times.cpp
+++ Recursion/print_n_times.cpp
@@ -2,9 +2,18 @@
 using namespace std ;
 
 // Recursion 
+void printno(int i, int N){
 
+    if(i == N){
+        return ;
+    }
+
+    cout << i ; 
+
+    printno(i++, N);
+}
 
 int main(){
-
+    printno(0, 5);
     return 0 ;
 }
